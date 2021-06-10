1c8e76b019f0a5d2c3bf78c8a08d023647cc09a5
Jason Katz <jkatz94@gmail.com>
2021-06-09 21:11:29 -0400

    Convert remaining files

--- iterm/install.sh
+++ iterm/install.sh
@@ -3,5 +3,5 @@
 mkdir ~/Library/Application\ Support/iTerm2/DynamicProfiles/ > /dev/null 2>&1
 
 ITERM_DYNAMIC_PROFILES_PATH="$PWD/iterm-dynamic-profiles.json"
-ITERM_DYNAMIC_PROFILES_DEST=~/Library/Application\ Support/iTerm2/DynamicProfiles/toolkit-dynamic-profiles.json
+ITERM_DYNAMIC_PROFILES_DEST=~/Library/Application\ Support/iTerm2/DynamicProfiles/dotfiles-dynamic-profiles.json
 install_file $ITERM_DYNAMIC_PROFILES_PATH "$ITERM_DYNAMIC_PROFILES_DEST"
