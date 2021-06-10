1b5638f0e34d8c15ebcb1502db90cd81feaf887d
Jason Katz <jkatz94@gmail.com>
2021-06-09 21:17:30 -0400

    Fix vim scripts

--- vim/uninstall.sh
+++ vim/uninstall.sh
@@ -4,7 +4,7 @@ VIMRC_PATH="$PWD/.vimrc"
 uninstall_file $VIMRC_PATH ~/$(basename $VIMRC_PATH)
 
 DOCKERFILE_VIM_PATH="$PWD/vimfiles/syntax/dockerfile.vim"
-uninstall_file $DOCKERFILE_VIM_PATH ~/.vim/syntax
+uninstall_file $DOCKERFILE_VIM_PATH ~/.vim/syntax/$(basename $DOCKERFILE_VIM_PATH)
 
 MONOKAI_VIM_PATH="$PWD/vimfiles/colors/monokai.vim"
-uninstall_file $MONOKAI_VIM_PATH ~/.vim/colors
+uninstall_file $MONOKAI_VIM_PATH ~/.vim/colors/$(basename $MONOKAI_VIM_PATH)
