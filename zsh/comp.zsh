eae464799fda086d98561bf0d9e42aa9e12aa397
Jason Katz <jkatz94@gmail.com>
2021-09-02 15:33:03 -0400

    Improve zsh autosuggestion bindings

--- zsh/comp.zsh
+++ zsh/comp.zsh
@@ -1,5 +1,10 @@
 autoload -Uz compinit && compinit
 
+ZSH_AUTOSUGGEST_STRATEGY=(completion history)
+
+bindkey "^I" autosuggest-accept
+bindkey "\x1b" autosuggest-clear
+
 # The following lines were added by compinstall
 zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}'
