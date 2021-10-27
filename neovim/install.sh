a7bc281e5e945b42623c9cd3b1f70c451e88ee83
Jason Katz <jkatz94@gmail.com>
2021-10-27 17:19:34 -0400

    Fix neovim config

--- neovim/install.sh
+++ neovim/install.sh
@@ -4,3 +4,6 @@ mkdir -p ~/.config/nvim
 
 NVIMINIT_PATH="$PWD/init.vim"
 install_file $NVIMINIT_PATH ~/.config/nvim
+
+COCSETTINGS_PATH="$PWD/coc-settings.json"
+install_file $COCSETTINGS_PATH ~/.config/nvim
--- neovim/uninstall.sh
+++ neovim/uninstall.sh
@@ -2,3 +2,6 @@
 
 NVIMINIT_PATH="$PWD/init.vim"
 uninstall_file $NVIMINIT_PATH ~/.config/nvim/$(basename $NVIMINIT_PATH)
+
+COCSETTINGS_PATH="$PWD/init.vim"
+uninstall_file $COCSETTINGS_PATH ~/.config/nvim/$(basename $COCSETTINGS_PATH)
