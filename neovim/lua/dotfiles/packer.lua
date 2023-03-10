9b819f91eb2843acd485fed29f0e50d7edfbc312
Jason Katz <jkatz94@gmail.com>
2023-03-10 12:50:17 -0500

    Cleanup neovim config

--- neovim/lua/dotfiles/packer.lua
+++ neovim/lua/dotfiles/packer.lua
@@ -1,5 +1,4 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
-print('packer')
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
